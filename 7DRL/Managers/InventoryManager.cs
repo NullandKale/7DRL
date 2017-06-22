5b603fd558320715902d6638c015ea328c3a3424
Francis Zinsli <zinsli.alec@gmail.com>
2017-06-22 00:06:51 -0700

    Started adding player inventory

---
+++ 7DRL/Managers/InventoryManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DRL.Managers
+{
+    public class InventoryManager
+    {
+
+    }
+
+    public class Item
+    {
+        public bool isOnGround;
+        public int xPos;
+        public int yPos;
+
+        public int weight;
+        public int value;
+    }
+}
